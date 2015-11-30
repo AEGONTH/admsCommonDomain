575a403b143a04f5c50977f129da71dccb9ac8b4
Patawee Chaiarksornwech <moth.pc@gmail.com>
2015-11-30 15:31:34 +0700

    Update
    - comment method removed

--- src/main/java/com/adms/common/entity/UserLogin.java
+++ src/main/java/com/adms/common/entity/UserLogin.java
@@ -74,37 +74,17 @@ public class UserLogin extends BaseDomain {
 		return username;
 	}
 	
-//	public void setUsername(String username) {
-//		this.username = username;
-//	}
-	
 	public String getPwd() {
 		return pwd;
 	}
 	
-//	public void setPwd(String pwd) {
-//		this.pwd = pwd;
-//	}
-
-//	public void setRole(String role) {
-//		this.role = role;
-//	}
-
 	public Date getLoginDate() {
 		return loginDate;
 	}
 
-//	public void setLoginDate(Date loginDate) {
-//		this.loginDate = loginDate;
-//	}
-
 	public Boolean getLoginSuccess() {
 		return loginSuccess;
 	}
-
-//	public void setLoginSuccess(Boolean loginSuccess) {
-//		this.loginSuccess = loginSuccess;
-//	}
 
 	public Map<String, List<String>> getRolePrivileges() {
 		return rolePrivileges;
@@ -118,8 +98,4 @@ public class UserLogin extends BaseDomain {
 		this.newPwd = newPwd;
 		return this;
 	}
-
-//	public void setRolePrivileges(Map<String, List<String>> rolePrivileges) {
-//		this.rolePrivileges = rolePrivileges;
-//	}
 }
